6e63954a9297d5fc7f7d1cde810468d1440c558a
Rob Grant <rob.grant@nanoporetech.com>
2024-12-13 16:43:27 +0000

    Slightly shorter day 5
    Signed-off-by: Rob Grant <rob.grant@nanoporetech.com>

--- day5/part_1_2.py
+++ day5/part_1_2.py
@@ -10,27 +10,23 @@ def parse(raw_input):
         rules[(prepage, postpage)] = 1
         rules[(postpage, prepage)] = -1
     
-    rules_key_function = cmp_to_key(lambda a, b: rules.get((a, b), rules.get((b, a), 0)))    
     pages = [raw_page.split(",") for raw_page in raw_pages.split("\n")]
+    rules_key_function = cmp_to_key(lambda a, b: rules.get((a, b), rules.get((b, a), 0)))  
 
-    return rules_key_function, pages
-
-
-def filter_pages(rules_key_function, pages, valid_or_not):
-    return [
-        page for page in pages
-        if (sorted(page, key=rules_key_function, reverse=True) == page) is valid_or_not
-    ]
+    valids = [page for page in pages if (sorted(page, key=rules_key_function, reverse=True) == page)]
+    invalids = [page for page in pages if (sorted(page, key=rules_key_function, reverse=True) != page)]
+  
+    return valids, invalids, rules_key_function
 
 
-def part1(rules_key_function, pages):
-    return sum(int(valid[len(valid)//2]) for valid in filter_pages(rules_key_function, pages, True))
+def part1(valids, invalids, rules_key_function):
+    return sum(int(valid[len(valid)//2]) for valid in valids)
 
 
-def part2(rules_key_function, pages):
+def part2(valids, invalids, rules_key_function):
     return sum(
         int((new_invalid := sorted(invalid, key=rules_key_function, reverse=True))[len(new_invalid) // 2])
-        for invalid in filter_pages(rules_key_function, pages, False)
+        for invalid in invalids
     )
 
 
